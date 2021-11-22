105e85464a3937e779157ebb96e88ce0bd4dba84
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2021-11-22 23:28:35 +0300

    Update syntax.py

--- Python/syntax.py
+++ Python/syntax.py
@@ -48,28 +48,6 @@ s*5
 str(x)
 
 print("\n")
-#Lists
-names=["bark","dark","fark"]
-nums=[1,2,3,5,4]
-
-names.append('murphy')#Appends(adding at the end)
-names.insert(2,'aretha')#Inserts at index
-
-print("Concatenation of the lists",names+nums)
-nums*3 #Not a mathematical operation(use numpy for that)
-
-names.index('bark') #Returns index
-names.remove('fark') #or del names[2]
-
-for name in names:
-    print(name)
-
-nums.sort(reverse=True)
-print(nums)
-nums.sort()
-print(nums)
-
-print("\n")
 #File I/O
 
 #Open for read
@@ -104,4 +82,53 @@ def sum(a,b):
 print(sum(1,2))#Calling a function
 #If you write no return, it will return None
 #If you write return with multiple elements
-#It will be return tuple
+#It will  return tuple
+
+
+print("\n")
+#Reading from url
+import urllib.request
+with urllib.request.urlopen("http://www.python.org") as url:
+    u = url.read()
+    #print(u) #650 lines of html code :)
+
+print('\n')
+#Lists
+names=["bark","dark","fark"]
+nums=[1,2,3,5,4]
+
+names.append('murphy')#Appends(adding at the end)
+names.insert(2,'aretha')#Inserts at index
+
+print("Concatenation of the lists",names+nums)
+nums*3 #Not a mathematical operation(use numpy for that)
+
+names.index('bark') #Returns index
+names.remove('fark') #or del names[2]
+
+for name in names:
+    print(name)
+
+nums.sort(reverse=True)
+print(nums)
+nums.sort()
+print(nums)
+
+print('\n')
+#Tuples
+#You can not modfiy tuples
+#in this case t gets deleted and gets recreated
+t=()#Empty tuple
+print(t)
+t=('Meta',)
+print(t)
+t=('GOOG',100,490.1)
+print(t)
+
+print(t[0])
+
+#t[1]=75 #Tuples can't be modified
+
+t2 = (t[0], 75, t[2]) #You can also create new one with other tuples values
+print(t2)
+
