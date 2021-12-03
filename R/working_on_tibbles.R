da58971db622fcd8fe61e8640f51a6b4adac189d
cbarkinozer <43732258+cbarkinozer@users.noreply.github.com>
2021-12-03 15:50:10 +0300

    Update working_on_tibbles.R

--- R/working_on_tibbles.R
+++ R/working_on_tibbles.R
@@ -1,54 +1,54 @@
 library(tidyverse)
 
-#Creates a table with a,b,c are col names, and rest as row
+#Creates a table with a,b,c as column names, and rest as rows
 # to read a csv (comma separated) file from a directory
 ourdata<-read_csv("data/heightsdata.csv")
 ourdata
 
-# to read a csv (semicolon separated) file from a directory
+# read a csv (semicolon separated) file from a directory
 ourdata<-read_csv2("data/heightsdata.csv")
 ourdata
 
-# to read from an inline CSV file
+# create a CSV file
 read_csv("a,b,c
 1,2,3
 4,5,6")
 
-# to skip the first 2 line when reading from a file
+# skip the first 2 line when reading from a file
 read_csv("The first line of metadata
 The second line of metadata
 x,y,z
-1,2,3", skip = 2) #Skip first rows
+1,2,3", skip = 2) #Skip first 2 rows
 
-# or to skip a line starting with a symbol
+# skip a line starting with a symbol
 read_csv("# A comment I want to skip
 x,y,z
 1,2,3", comment = "#") #Skip by symbol
 
-# if the column names are missing, we can assign the names as X1,..,Xn
+# if the column names are missing, we can automatically assign the names as X1,..,Xn
 read_csv("1,2,3
          4,5,6", col_names = FALSE)
 
 # "\n" is a convenient shortcut for adding a new line.
 read_csv("1,2,3\n4,5,6", col_names = FALSE)
 
-# to assing the column names, a character vector can be used
+# assing the column names, a character vector can be used
 read_csv("1,2,3\n4,5,6", col_names = c("x", "y", "z"))
 
-# to assign the missing values as NA
+# assign the missing values as NA
 x<-read_csv("a,b,c\n1,2,.\nA,3,4", na = ".") #Make "." as NA
 x
 
-# to check values whether they are integer values or not
+# check values whether they are integer values or not
 parse_integer(x$a)
 
 parse_logical(c("TRUE", "FALSE", "T", "1", "9"))
 parse_date(c("2020-12-03", "2020-26-11"))
 
-# checking the values when you create a vector
+# check the values when you create a vector
 y <- parse_integer(c("123", "345", "abc", "123.45"))
 
-# to see all parsing failures, use problems()
+# see all parsing failures, use problems()
 problems(y)
 
 # working with a large file, you may have lots of parsing failures
@@ -64,7 +64,7 @@ challenge <- read_csv(
   )
 )
 
-# to figure out the type of column y
+# figure out the type of column y
 guess_parser(challenge$y)
 
 # change the column specification of column y to date
@@ -76,20 +76,20 @@ challenge <- read_csv(
   )
 )
 
-# to read the other Excel formats, use readxl library
+# read the other Excel formats, use readxl library
 library(readxl)
 
-# to read an xls or xlsx file, use read_excel funtion
+# read an xls or xlsx file, use read_excel funtion
 xlsx_example <- readxl_example("datasets.xlsx")
 read_excel(xlsx_example)
 
-# to get the list of sheet names
+# get the list of sheet names
 excel_sheets(xlsx_example)
 
-# to read data from a sheet
+# read data from a sheet
 read_excel(xlsx_example, sheet = "mtcars")
 
-# to write your data to a csv file
+# write your data to a csv file
 write_csv(challenge, "challenge.csv")
 
 # datasets with different data organization
@@ -102,7 +102,7 @@ table4b
 # untidy data example
 table4a
 
-# to make it tidy, use gather() function
+# make it tidy, use gather() function
 table4a %>%
   gather(`1999`, `2000`, key = "year", value = "cases")
 
@@ -110,7 +110,7 @@ table4a %>%
 table4b %>%
   gather(`1999`, `2000`, key = "year", value = "population")
 
-# To combine the tidied datasets into a single tibble, use dplyr::left_join()
+# combine the tidied datasets into a single tibble, use dplyr::left_join()
 tidy4a <- table4a %>%
   gather(`1999`, `2000`, key = "year", value = "cases")
 tidy4b <- table4b %>%
@@ -120,24 +120,22 @@ left_join(tidy4a, tidy4b)
 # observations in multiple rows
 table2
 
-# to spread rows into columns
+# spread rows into columns
 spread(table2, key = type, value = count)
 
 # data having one column containing two variables
 table3
 
-# to separate these two variables
+# separate these two variables
 table3 %>%
   separate(rate, into = c("cases", "population"), sep = "/")
 
-# to convert the data types to correct ones
+# convert the data types to correct ones
 table3 %>%
   separate(rate, into = c("cases", "population"),
-    convert = TRUE
+           convert = TRUE
   )
 
-# to separate variables according to a specified position
+# separate variables according to a specified position
 table3 %>%
   separate(year, into = c("century", "year"), sep = 2)
-
-
