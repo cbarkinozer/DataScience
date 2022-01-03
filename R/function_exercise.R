2113dda68a62eff3e855cef169802fb63fc63f97
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-03 20:40:53 +0300

    Create function_exercise.R

---
+++ R/function_exercise.R
@@ -0,0 +1,60 @@
+set.seed(2018556059)
+
+#1.Write a function that finds a prime number(s) given a set of numbers.
+#Your function also provides prime factors of non-prime numbers.
+#The function you will write should return the prime number(s) given in this vector and
+#the non-prime number(s) with their prime factors.
+
+
+#For example you have a vector of numbers like (89, 107, 597, 931, 1083).
+#Your result will be Prime numbers : 89 107
+#Non-prime numbers : 597 [3 199]  931 [7 7 19] 1083 [3 19 19]
+
+prime_vector<-c(89, 107, 597, 931, 1083)
+
+flag = 0
+is.prime <- function(prime_vector) {
+  
+  prime<-vector()
+  non_prime<-vector()
+  
+  divider<-0
+  for(x in prime_vector ){
+    
+    if(x > 1) {
+      flag = 1
+      for(i in 2:(x-1)) {
+        if ((x %% i) == 0) {
+          flag = 0
+          divider=i
+          break
+        }
+      }
+    } 
+    if(x == 2){flag = 1}
+    if(flag == 1) {
+      prime<-c(prime,x)
+    } else {
+      non_prime<-c(non_prime,x)
+      non_prime<-c(non_prime,divider)
+    }
+  
+  }
+print(paste("Prime numbers :", prime))
+print("Non-prime numbers: ")
+print(non_prime)
+} 
+
+is.prime(prime_vector)
+
+
+#2. Write a function that finds the letter numbers of all words in a given text and sorts the text according to those numbers from words with few letters to words with many letters.
+#Sort the words containing the same number of letters alphabetically. For having text, you may use Sentences in tidyverse.
+#Select 5 or 6 sentences randomly from Sentences. For example, you have these sentences. 
+#[1] "We find joy in the simplest things. He wrote down a long list of items.
+#The hail pattered on the burnt brown grass.
+#Screen the porch with woven straw mats.
+#The theft of the pearl pin was kept secret.
+#Sweet words work better than fierce."
+#Your function should return the result like this
+# [1] "a he in of of on we joy pin the the the the the the was down find hail kept list long mats than with work brown burnt grass items pearl porch straw sweet theft words woven wrote better fierce screen secret things pattered simplest"
