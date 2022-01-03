e9ee348af4294368b5a2f65ae0e16872476904fa
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-03 20:46:20 +0300

    Update string_operations.R

--- R/string_operations.R
+++ R/string_operations.R
@@ -1,172 +1,61 @@
-install.packages("tidyverse")
-library(tidyverse)
+set.seed(2018556059)
 
-str1<-"This is a string"
-string2<-"To put 'quote', use single quote"
-
-"\u00b5" #special unicode symbol
-
-?"'" #Show options
-
-str_length(c("a","R for data science",NA)) #Length of these values
-
-str_c("x","y","z") #concatenate
-
-str_c("Letter",seq_along(letters),letters,sep=":")
-
-str_c(str_c(seq_along(letters),"Letter"),letters,sep=":")
-
-x<-c("abc",NA)
-str_c("|-",x,"-|")#Adding variable inside string
-
-str_c("|-",str_replace_na(x),"-|")
+#1.Write a function that finds a prime number(s) given a set of numbers.
+#Your function also provides prime factors of non-prime numbers.
+#The function you will write should return the prime number(s) given in this vector and
+#the non-prime number(s) with their prime factors.
 
 
-name<-"Data"
-time_of_day<-" Evening"
-birthday<-TRUE
-str_c(
-  "Good", time_of_day," ",name,",",
-  if(birthday) " Happy Birthday"
-)
+#For example you have a vector of numbers like (89, 107, 597, 931, 1083).
+#Your result will be Prime numbers : 89 107
+#Non-prime numbers : 597 [3 199]  931 [7 7 19] 1083 [3 19 19]
 
-str_c(letters,collapse=",") #letters are defined as all letters in alphabet
+prime_vector<-c(89, 107, 597, 931, 1083)
 
-x<-c("Apple","Banana","Pear")
+flag = 0
+is.prime <- function(prime_vector) {
+  
+  prime<-vector()
+  non_prime<-vector()
+  
+  divider<-0
+  for(x in prime_vector ){
+    
+    if(x > 1) {
+      flag = 1
+      for(i in 2:(x-1)) {
+        if ((x %% i) == 0) {
+          flag = 0
+          divider=i
+          break
+        }
+      }
+    } 
+    if(x == 2){flag = 1}
+    if(flag == 1) {
+      prime<-c(prime,x)
+    } else {
+      non_prime<-c(non_prime,x)
+      non_prime<-c(non_prime,paste("[",divider,"]"))
+    }
+  
+  }
+print("Prime numbers: ")
+print(prime)
+print("Non-prime numbers: ")
+print(non_prime)
+} 
 
-str_sub(x,1,3) #substring
-
-str_sub(x,-3,-1)
-
-str_sub("a",1,5) #Don't give index out of bound error just gives result
-
-str_sub(x,1,1)<-str_to_lower(str_sub(x,1,1))
-x
-
-str_to_upper(c("ý","i"),locale="tr") #Turkish
-x<-c("inci","ýþýk","ýslak")
-str_sort(x,locale="tr")
-
-install.packages("htmlwidgets")
-library(htmlwidgets)
-x<-c("Apple","Banana","Pear")
-str_view(x,"an")
-str_view(x,".a.")#There are letters before and after a
-str_view_all(x,"an")
-
-str_view(c("abc","a.c","def"),"\\.") #To search . in string
-str_view(c("a\\b"),"\\\\") #To search \ in string
-
-str_view(x,"^A")#To search  starts with A
-str_view(x,"a$")#To search ends with a
-
-x<-c("apple pie","apple","apple cake")
-str_view(x,"^apple$") #I just want apple so I show start and end
-
-x<-c("Apple","Banana","Pear")
-sum(str_detect(x,"e")) #Find how many of them has it
-#detect turns boolean
-
-sum(str_detect(words,"^t")) #Show count
-
-mean(str_detect(words,"[aeiou]$")) #%27 has it
-
-no_vovels<-!str_detect(words,"[aeiou]")
-no_vovels_2<-str_detect(words,"^[^aeiou]+$")
-
-identical(no_vovels,no_vovels_2)#They are identical
-
-words[no_vovels_2] #Ones with no vowels
-
-words[str_detect(words,"x$")]
-str_subset(words,"x$")
-
-df<-tibble(
-  word=words,
-  i=seq_along(word)
-)
-df%>%
-  filter(str_detect(words,"x$"))
+is.prime(prime_vector)
 
 
-str_count(x,"a")#how many a char exist?
-
-mean(str_count(words,"[aeiou]"))
-
-df %>%
-  mutate(
-    vovels =str_count(word,"[aeiou]"),
-    consonants= str_count(word,"[^aeiou]")
-  )
-
-str_count("abababa","aba")
-str_view_all("abababa","aba")
-
-
-colors<-c(" red "," orange "," yellow "," green "," blue "," purple "," brown ")
-color_match<-str_c(colors,collapse="|")
-
-
-has_color<-str_subset(sentences,color_match) #Find sentences who has colors in them
-
-str_extract(has_color,color_match)
-
-more<-sentences[str_count(sentences,color_match)>1]
-str_view_all(more,color_match)
-
-str_extract_all(more,color_match,simplify = TRUE)
-
-x<-c("a","a b","a b c")
-str_extract_all(words,"[a-z]",simplify = TRUE)
-
-noun<-"(a|the|an|A|The) ([^ ]+)"
-has_noun<-sentences %>%
-  str_subset(noun) %>%
-  head(10)
-
-has_noun %>% 
-  str_extract(noun)
-
-has_noun %>% 
-  str_match(noun)
-
-tibble(sentence=sentences) %>%
-  extract(
-    sentence,c("article","noun","(a|the) ([^ ]+)"),
-    remove=FALSE
-  )
-
-has_noun %>%
-  str_match_all(noun)
-
-x<-c("Apple","Banana","Pear")
-str_replace(x,"[aeiou]","-")
-str_replace_all(x,"[aeiou]","-")
-
-sentences %>%
-  head(5) %>%
-  str_split(" ")
-
-"a|b|c|d" %>%
-  str_split("\\|")%>%
-  .[[1]]
-
-sentences %>%
-  head(5) %>%
-  str_split(" ",simplify=TRUE)
-
-sentences %>%
-  head(5) %>%
-  str_split(" ") %>%
-  simplify()
-
-
-fields<-c("Name:Hadley:ADC","Country:NZ","Age:35")
-fields %>%
-  str_split(":",simplify=TRUE)
-
-x<-"This is a sentence. This is another sentence"
-
-str_view_all(x,boundary("word"))
-str_split(x," ")
-str_split(x,boundary("word"))
+#2. Write a function that finds the letter numbers of all words in a given text and sorts the text according to those numbers from words with few letters to words with many letters.
+#Sort the words containing the same number of letters alphabetically. For having text, you may use Sentences in tidyverse.
+#Select 5 or 6 sentences randomly from Sentences. For example, you have these sentences. 
+#[1] "We find joy in the simplest things. He wrote down a long list of items.
+#The hail pattered on the burnt brown grass.
+#Screen the porch with woven straw mats.
+#The theft of the pearl pin was kept secret.
+#Sweet words work better than fierce."
+#Your function should return the result like this
+# [1] "a he in of of on we joy pin the the the the the the was down find hail kept list long mats than with work brown burnt grass items pearl porch straw sweet theft words woven wrote better fierce screen secret things pattered simplest"
