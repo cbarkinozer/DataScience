9d22bb31f6e387161f07b95c8d19eaada210a998
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-01-06 16:35:50 +0300

    Update pandas_lib.py

--- Python/pandas_lib.py
+++ Python/pandas_lib.py
@@ -111,4 +111,205 @@ print()
 
 
 #Reading Files
-titles=pd.read_csv('',index_col=None,encoding='utf-8')
+titles=pd.read_csv('C:\\Users\\barkin\\Downloads\\titles.csv',index_col=None,encoding='utf-8')
+casts=pd.read_csv('C:\\Users\\barkin\\Downloads\\cast.csv',index_col=None,encoding='utf-8')
+print(len(titles))
+print(titles.head(3))
+print()
+
+#Selecting a row or column from DataFrame, makes it a series
+print(titles.iloc[0])
+print()
+
+#Data can be filtered by providing some boolean expression in DataFrame
+after85=titles[titles['year']>1985]
+print(after85.head())
+print()
+
+t=titles
+movies90=t[(t['year']>1990)& (t['year']<2000)]
+print(movies90.head())
+print()
+
+#Sorting
+#Can be performed using sort_index for index
+#sort_values for selected column
+
+t=titles
+macbeth=t[t['title']=='Macbeth'].sort_index()
+print(macbeth.head())
+print()
+
+macbeth=t[t['title']=='Macbeth'].sort_values('year')
+print(macbeth.head())
+print()
+
+#Some data can contain not a numeric values NaN as null
+print(casts.iloc[3:5]) #Note: ix is deprecated
+print()
+
+#Displaying nulls
+print(casts[casts['n'].isnull()].head(3))
+print()
+
+#NaN can be fill
+c_fill=casts[casts['n'].isnull()].fillna('NA')
+c_fill.head(2)
+
+t=titles
+print(t[t['title']=='Maa'])
+print()
+
+#All the movies starts with Maa
+print(t[t['title'].str.startswith("Maa ")].head(3))
+print()
+#Total number of occurrences can be counted using value_counts()
+print(t['year'].value_counts().head())
+print()
+
+#Plots
+#Pandas supports the matplotlib library cand can be used to plot
+#the data as well
+import matplotlib.pyplot as plt
+t=titles
+p=t['year'].value_counts()
+p.plot()
+#plt.show()
+#The plot that is generated from above code is not useful
+#We need to sort it first
+
+p.sort_index().plot()
+#plt.show()
+
+#Groupby
+#Data can be grouped by columns-headers
+
+cg= casts.groupby(['year']).size()
+cg.plot()
+#plt.show()
+
+#We can take multiple parameters for grouping
+c=casts
+cf=c[c['name']=='Aaron Abrams']
+print(cf.groupby(['year']).size().head())
+print()
+
+#Grouping based on max ratings in a year
+print(cf.groupby(['year']).n.max().head())
+print()
+
+#File operations
+#Reading files
+#There are 3 methods that has different reading parameters
+
+#pd.read_csv for just reading
+df=pd.read_csv('C:\\Users\\barkin\\Downloads\\titles.csv')
+print(df)
+print()
+
+#pd.DataFrame.from_csv is deprecated
+
+#pd.read_table for reading with seperation symbol defined
+df=pd.read_table('C:\\Users\\barkin\\Downloads\\titles.csv',sep=',')
+print(df)
+print()
+
+#If header is none, default headers will be numeric indexes
+a=pd.read_csv('C:\\Users\\barkin\\Downloads\\titles.csv',header=None)
+print(a)
+print()
+
+#You can specify using "names"
+b= pd.read_csv('C:\\Users\\barkin\\Downloads\\titles.csv',names=['a','b','c','d','message'])
+print(b)
+print()
+
+#You can specify column that has the row index
+c=pd.read_csv('C:\\Users\\barkin\\Downloads\\titles.csv',names=['a','b','c','d','message'],index_col='message')
+print(c)
+print()
+
+
+#Writing data to a file
+c.to_csv('d_out.csv')
+#Save without headers
+c.to_csv('d_out.csv',header=False,index=False)
+
+
+#EXERCISE
+#Find top 10 years with the highest number of films
+casts=pd.read_csv('C:\\Users\\barkin\\Downloads\\cast.csv',index_col=None,encoding='utf-8')
+c=casts.groupby('year').size().sort_values(ascending=False).head(10)
+print(c)
+print()
+
+#Find the top 10 characters with the highest score
+c2=casts.groupby('character').mean()
+print(c2['n'].sort_values(ascending=False).head(10))
+print()
+
+
+#Concatenating the data
+s1= pd.Series([0,1],index=['a','b'])
+s2=pd.Series([2,1,3],index=['c','d','e'])
+s3=pd.Series([4,7],index=['a','e'])
+
+print(s1)
+print()
+print(s2)
+print()
+print(s3)
+print()
+
+print(pd.concat([s1,s2],axis=1))
+print()
+
+#To identify different pieces of concatenate operation. We provide keys.
+print(pd.concat([s1,s2,s3],keys=['one','two','three']))
+print()
+
+
+#Data transformation
+#Removing duplicates
+data={'name':['metan','IBBM','GOGO','GOGO'],
+   'date':['2021-10-08','2012-02-10','2010-04-09','2010-04-09'],
+   'shares':[100,30,90,90],
+   'price':[10.2,12.3,10.1,10.1]}
+df=pd.DataFrame(data)
+print(df.duplicated())#Checking duplicates
+df2=df.drop_duplicates()
+print(df2) #Deleting duplicates
+print()
+#If there are multiple duplicates the last one also get drop you can add parameter keep="last"
+#You can also drop column specific as df.drop_duplicates(['k1'])
+
+#Replacing values
+df3=df2.replace('IBBM','IMB')
+print(df3)
+print()
+
+#IMB->BIM, GOGO->BOGO
+df4=df3.replace(['IMB','GOGO'],['BIM','BOGO'])
+
+print(df4)
+print()
+
+#Arguments can be passsed as dictionary as well
+df4= df4.replace({'BIM':'IMB','BOGO':'BOGOS'})
+print(df4)
+print()
+
+#We can groupby first something than other thing
+df5=df4.groupby([df['shares'],df['price']])
+
+#Iterating over group
+#groupby supports iteration for 2 values
+for shares,price in df5:
+    print(shares)
+    print(price)
+
+#Data aggregation
+#Data aggregations are applicable on grouped data
+print()
+print(df5.max())
+print(df5.min())
