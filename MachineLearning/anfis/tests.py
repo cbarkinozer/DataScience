bbb5526869034337e31cb7fb08da460588526612
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2022-04-13 19:34:58 +0300

    Anfis updated version for python 3
    Anfis updated version for python 3

---
+++ MachineLearning/anfis/tests.py
@@ -0,0 +1,22 @@
+import anfis
+import membership #import membershipfunction, mfDerivs
+import numpy
+
+ts = numpy.loadtxt("trainingSet.txt", usecols=[1,2,3])#numpy.loadtxt('c:\\Python_fiddling\\myProject\\MF\\trainingSet.txt',usecols=[1,2,3])
+X = ts[:,0:2]
+Y = ts[:,2]
+
+mf = [[['gaussmf',{'mean':0.,'sigma':1.}],['gaussmf',{'mean':-1.,'sigma':2.}],['gaussmf',{'mean':-4.,'sigma':10.}],['gaussmf',{'mean':-7.,'sigma':7.}]],
+            [['gaussmf',{'mean':1.,'sigma':2.}],['gaussmf',{'mean':2.,'sigma':3.}],['gaussmf',{'mean':-2.,'sigma':10.}],['gaussmf',{'mean':-10.5,'sigma':5.}]]]
+
+
+mfc = membership.membershipfunction.MemFuncs(mf)
+anf = anfis.ANFIS(X, Y, mfc)
+anf.trainHybridJangOffLine(epochs=20)
+print round(anf.consequents[-1][0],6)
+print round(anf.consequents[-2][0],6)
+print round(anf.fittedValues[9][0],6)
+if round(anf.consequents[-1][0],6) == -5.275538 and round(anf.consequents[-2][0],6) == -1.990703 and round(anf.fittedValues[9][0],6) == 0.002249:
+	print 'test is good'
+anf.plotErrors()
+anf.plotResults()
